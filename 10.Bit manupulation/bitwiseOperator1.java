b67bad26f69fdec483b932c1206d598c23251f6f
shashank raj <rshashank608@gmail.com>
2024-09-19 20:47:17 +0530

    first commit of bitwise

---
+++ 10.Bit manupulation/bitwiseOperator1.java
@@ -0,0 +1,23 @@
+public class bitwiseOperator1{
+
+    public static void main(String[] args){
+
+        System.out.println(5|9); // 13
+        System.out.println(5&9); // 1
+    //check byte int size
+        // byte x= 254;
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Short.MAX_VALUE);
+        System.out.println(Long.MAX_VALUE);
+
+        System.out.println(Byte.MIN_VALUE);
+
+        // xor operator
+
+        System.out.println(0^0);
+
+        //1's compliment
+        System.out.println(~6);
+    }
+}
