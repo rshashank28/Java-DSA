9d8b190590c72e5bfc817f7170e5e54aae5f5b42
shashank raj <rshashank608@gmail.com>
2024-09-21 13:13:10 +0530

    bit

---
+++ 10.Bit manupulation/ShiftOperator2.java
@@ -0,0 +1,13 @@
+public class ShiftOperator2 {
+
+    
+    public static void main(String[] args) {
+        
+        System.out.println(91>>2); //number of bit shift is 2 and ans will be 22
+        System.out.println(91<<2); // ans will be 364
+
+
+        byte x= 102,y=3;
+        System.out.println(y<<3);
+    }
+}
