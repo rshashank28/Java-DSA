3ca5c6e4ff22c6bda2b87eeea6365ccfa60d41e7
Shashank raj <144523360+rshashank28@users.noreply.github.com>
2024-10-05 18:46:10 +0530

    Update Leetcode2640.java

--- prefixSum/Leetcode2640.java
+++ prefixSum/Leetcode2640.java
@@ -1,5 +1,25 @@
 package prefixSum;
 
 public class Leetcode2640 {
+    class Solution {
+    public long[] findPrefixScore(int[] nums) {
+        int n=nums.length;
+        long [] conver=new long[n];
+
+    int max=nums[0];
+        for(int i=0;i<n;i++){
+            if(max < nums[i]) max=nums[i];
+            conver[i]=nums[i]+max;
+
+        }
+        for(int i=1;i<conver.length;i++){
+            conver[i]+=conver[i-1];
+        }
+        return conver;
+    }
+
+
+        
+}
     
 }
